6701c02f6fa1ed52968ddda5c3aa67c90429f192
Ryan Alyn Porter <rap@endymion.com>
2011-07-25 11:49:51 -0400

    Initial commit to basecamp-status.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasecampStatus::Application.config.secret_token = '70db3fa294190838bbf37077dc657492b125888aff1bc2aa0aa305bc30a7f2d2da83b90cd06d81986a93eb9156ac188976d193905a5850ae0d0bafba535bb1e3'
