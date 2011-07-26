d4a488f80aadaeb2c2308fd6215182a32414e5c1
Orlando Del Aguila <orlando@bakedweb.net>
2011-07-26 11:01:21 -0430

    first spyke - rev 1

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,9 @@
 class BoardsController < ApplicationController
   
   def index
-     b = Board.new
-     b.connection
-     @hash = b.generate_data_hash(b.get_todo_lists(b.get_all_projects_id.first).todo_items)
+     @b = Board.new
+     @b.connection
+     @p = @b.get_projects
   end
 
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -9,34 +9,15 @@ class Board
     Basecamp.establish_connection!(URL, APITOKEN, 'X', true)
   end
 
-  def get_all_projects
-    projects = Basecamp::Project.all
+  def get_projects
+    Basecamp::Project.all #array with projects
   end
 
-  def get_all_projects_id
-    projects_id = Basecamp::Project.all.map {|project| project.id}
+  def get_todos(project)
+    Basecamp::TodoList.all(project.id).flatten #array with todo lists from project
   end
 
-  def get_todo_lists(project_id)
-    todo_lists = Basecamp::TodoList.all(project_id).first
-  end
-
-  def items(todo_list)
-    todo_items = todo_list.todo_items
-  end
-
-  def generate_data_hash(todo_items)
-      hash = Hash.new
-    todo_items.each_with_index do |item,index|
-      hash[index] = Hash.new
-      hash[index].store("id",item.id)
-      hash[index].store("content",item.content)
-      #hash[index].store("responsible_party_id",item.responsible_party_id)
-      #hash[index].store("responsible_party_name",item.responsible_party_name)
-      hash[index].store("due_at",item.due_at)
-    end
-    return hash
-  end
+  
 
 end
 
