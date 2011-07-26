229af76d84aead4e68f86d55c3a0e84401985341
Orlando Del Aguila <orlando@bakedweb.net>
2011-07-26 14:45:18 -0430

    second spyke rev 1

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,23 +1,21 @@
 require 'basecamp'
 
-APITOKEN = "ec20d0d864592cdfee706ca466af4e3b13398454"
-URL = "hashlabs.basecamphq.com"
+APITOKEN = ENV['BASECAMP_TOKEN']
+URL = "angelmg.basecamphq.com"
 
 class Board
   
-  def connection
-    Basecamp.establish_connection!(URL, APITOKEN, 'X', true)
+  def connection(url = URL, token = APITOKEN)
+    Basecamp.establish_connection!(url, token, 'X', true)
   end
 
   def get_projects
-    Basecamp::Project.all #array with projects
+    Basecamp::Project.all
   end
 
   def get_todos(project)
-    Basecamp::TodoList.all(project.id).flatten #array with todo lists from project
+    Basecamp::TodoList.all(project.id).flatten
   end
-
-  
 
 end
 
