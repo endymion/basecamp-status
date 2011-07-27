0b8055a5bc08e77518497b641a0a9d3bb1c91358
Orlando Del Aguila <orlando@bakedweb.net>
2011-07-27 13:50:16 -0430

    removing /lib/generators/haml from source

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,8 @@
 require 'basecamp'
 
+ENV['BASECAMP_URL']="angelmg.basecamphq.com"
+ENV['BASECAMP_TOKEN']="fcc852785aee2af1756a4a4e7709385880013ce0"
+
 APITOKEN = ENV['BASECAMP_TOKEN']
 URL = ENV['BASECAMP_URL']
 
