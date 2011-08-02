9e3fd364273c99f3791e8df59293b1b76f41f2ba
Orlando Del Aguila <orlando@bakedweb.net>
2011-08-02 16:52:40 -0430

    first spyke: filtering tasks by person

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,10 @@
 require 'basecamp'
   
+  ENV['BASECAMP_TOKEN'] = "ec20d0d864592cdfee706ca466af4e3b13398454"
+  ENV['BASECAMP_URL'] = "hashlabs.basecamphq.com"
+  
   APITOKEN = ENV['BASECAMP_TOKEN']
   URL = ENV['BASECAMP_URL']
-
   
 class Board
   
@@ -17,6 +19,6 @@ class Board
   def get_todos(project)
     Basecamp::TodoList.all(project.id).flatten
   end
-
+  
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Filter by Person
+
+var names = [];
+
+function get_names(){
+  $("span.assigned-to").each(function(index) {
+    names[index] = $(this).text().trim();
+    names = $.unique(names);
+    names.length = names.length - 1;
+  });
+};
+
+function fill_select(){
+  $.each(names,function(index,value){
+    $("#names").append("<option value="+ (index + 1) +">"+value+"</option>");
+  });
+};
+
+function filter_by_name(){
+  name = names[$("#names").val()];
+  $("span.assigned-to").each(function(){
+    if ($(this).text().trim() != name){
+      $(this).parent().fadeOut('slow');
+    }
+    else{
+      $(this).parent().fadeIn('slow');
+    }
+  });
+};
+
+$(document).ready(function(){
+  get_names();
+  fill_select();
+  $('#names').change(function() {
+    filter_by_name();
+  });
+});
+
+
