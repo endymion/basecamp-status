7327e4987dbebf04428c10ff7554c537c6b70ca5
Orlando Del Aguila <orlando@bakedweb.net>
2011-08-02 12:30:51 -0430

    varnish with 5 min cache

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,7 @@
 class BoardsController < ApplicationController
 
   def index
-    response.headers['Cache-Control'] = 'public, max-age=180'
+    response.headers['Cache-Control'] = 'public, max-age=300'
     @time = Time.now.strftime("%D - %T")
     @b = Board.new
     @b.connection
