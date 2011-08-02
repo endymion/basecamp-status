ec92f4407cb7471ed40edb343f143c480cd74743
Orlando Del Aguila <orlando@bakedweb.net>
2011-08-02 12:23:08 -0430

    varnish heroku

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,11 @@
 class BoardsController < ApplicationController
 
   def index
-     @b = Board.new
-     @b.connection
-     @p = @b.get_projects
-     response.headers['Cache-Control'] = 'public, max-age=180'
-     @time = Time.now.strftime("%D - %T")
+    response.headers['Cache-Control'] = 'public, max-age=180'
+    @time = Time.now.strftime("%D - %T")
+    @b = Board.new
+    @b.connection
+    @p = @b.get_projects
   end
 
 end
