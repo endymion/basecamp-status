6049f071204b33754fae0e4060d7bc9c363aa490
Orlando Del Aguila <orlando@bakedweb.net>
2011-08-02 11:29:32 -0430

    heroku cache

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,10 @@
 class BoardsController < ApplicationController
-caches_action :index 
-
 
   def index
      @b = Board.new
      @b.connection
      @p = @b.get_projects
+     response.headers['Cache-Control'] = 'public, max-age=300'
   end
 
 end
