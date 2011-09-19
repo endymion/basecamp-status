a1be71a37e42b97bf8583596831602e847f5b494
Hugo Rincon <hugo@bakedweb.net>
2011-09-19 14:07:21 -0430

    paginate todos

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
 
   def index
     response.headers['Cache-Control'] = 'public, max-age=300'
-    @todos = CachedTodo.all
+    page = !params[:page].nil? ? params[:page] : 1
+    @todos = CachedTodo.paginate({ :per_page => 200,  :page => page})
   end
 
 end
