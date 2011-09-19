691f0a41eebd09ec916a1d5175a1d03c89365c0b
Hugo Rincon <hugo@bakedweb.net>
2011-09-19 13:32:19 -0430

    refactor view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,10 +2,7 @@ class BoardsController < ApplicationController
 
   def index
     response.headers['Cache-Control'] = 'public, max-age=300'
-    @time = Time.now.strftime("%D - %T")
-    @b = Board.new
-    @b.connection
-    @p = @b.get_projects
+    @todos = CachedTodo.all
   end
 
 end
