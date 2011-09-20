777966bad26e55b71f77d31a4a13d5873a2b729e
Hugo Rincon <hugo@bakedweb.net>
2011-09-20 13:20:02 -0430

    mongo test db

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,3 +1,8 @@
-MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10038, { :logger => Rails.logger })
-MongoMapper.database = 'basecamp-status'
-MongoMapper.database.authenticate('basecamp', 'letmein')
+if ENV["RAILS_ENV"] == 'test'
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, { :logger => Rails.logger })
+  MongoMapper.database = 'basecamp-status' 
+else
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10038, { :logger => Rails.logger })
+  MongoMapper.database = 'basecamp-status'
+  MongoMapper.database.authenticate('basecamp', 'letmein')
+end
