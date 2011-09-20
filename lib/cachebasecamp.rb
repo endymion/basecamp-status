7b2c7508db477646530f45cc968a61e6a1f71419
Hugo Rincon <hugo@bakedweb.net>
2011-09-20 10:19:24 -0430

    refactor initialize method for basecamp calss

--- lib/cachebasecamp.rb
+++ lib/cachebasecamp.rb
@@ -1,12 +1,9 @@
 require 'basecamp'
 
-APITOKEN = ENV['BASECAMP_TOKEN'] || '74154e8fa88ade84971717cddb3e59fdb619800f'
-URL = ENV['BASECAMP_URL'] || 'angelmg.basecamphq.com'
-
 class Cachebasecamp
 
-  def initialize(url = URL, token = APITOKEN)
-    Basecamp.establish_connection!(url, token, 'X', true)
+  def initialize(url = ENV['BASECAMP_URL'], token = ENV['BASECAMP_TOKEN'])
+    Basecamp.establish_connection!(!url.nil? ? url : ENV['BASECAMP_URL'], !token.nil? ? token : ENV['BASECAMP_TOKEN'], 'X', true)
   end
   
   def get_projects
@@ -19,7 +16,7 @@ class Cachebasecamp
   
   def save_todos 
     get_projects.each do |project|
-      if project.status == "active"
+      if project.status == "active" && project.name != '*Creative Requests'
         get_todos(project.id).each do |todo|  
           todo.todo_items.each do |item|
             CachedTodo.new(:project_id => project.id, :todo_id => todo.id, :venue => split_project_name("venue", project.name), :event => split_project_name("event", project.name), :event_date => split_project_name("event_date", project.name), :todo_list => item.content, :todo_item => (todo.name.nil? ? todo.description : todo.name), :due_date => (!item.due_at.nil? ? item.due_at.strftime("%d %b %Y") : ''), :asigned_to => (item.responsible_party_name? ? item.responsible_party_name : '')).save
@@ -34,9 +31,9 @@ class Cachebasecamp
     colonPos = project_name.index(':')
       case part
         when "venue"
-          !dotPos.nil? ? project_name[0..(dotPos-4)] : ''
+          !dotPos.nil? ? project_name[0..(dotPos-4)] : 'N/D'
         when "event"
-          !colonPos.nil? ? project_name[(colonPos+2)..(project_name.length)] : ''
+          !colonPos.nil? ? project_name[(colonPos+2)..(project_name.length)] : 'N/D'
         when "event_date"
           project_name[/\d{1,2}[.]\d{1,2}[.]\d{1,2}/]
       end
