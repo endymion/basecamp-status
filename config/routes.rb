9f11401aa11b58533a610752cb0e5882b6c1dfe8
Hugo Rincon <hugo@bakedweb.net>
2011-09-20 18:47:15 -0430

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ BasecampStatus::Application.routes.draw do
  
   root :to => 'boards#index'
   resources :board
-
+ 
+  match "/board/persons" => "boards#get_person_by_project"
+  match "/board/persons/update" => "boards#update_todo_item_assigned"
 end
