932b35c53402d62060011e443409bc357dd45896
Hugo Rincon <hugo@bakedweb.net>
2011-09-20 19:21:02 -0430

    remove data

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,13 +9,13 @@ class BoardsController < ApplicationController
   end
   
   def get_person_by_project
-    @b_obj = Cachebasecamp.new('angelmg.basecamphq.com','74154e8fa88ade84971717cddb3e59fdb619800f')
+    @b_obj = Cachebasecamp.new
     persons = @b_obj.get_person_by_project(params[:project_id])
     render :json => persons
   end
   
   def update_todo_item_assigned
-    @b_obj = Cachebasecamp.new('angelmg.basecamphq.com','74154e8fa88ade84971717cddb3e59fdb619800f')
+    @b_obj = Cachebasecamp.new
     @b_obj.update_todo_item_assigned(params)
     item = CachedTodo.first(:item_id => params[:item_id].to_i)
     item.asigned_to = params[:responsible_party_name]
