b5f4e87460bd00580892959c905857fef8be4008
Hugo Rincon <hugo@bakedweb.net>
2011-10-06 11:17:01 -0430

    testing checkbox list on table filter

--- spec/javascripts/tableFilterSpec.js
+++ spec/javascripts/tableFilterSpec.js
@@ -22,10 +22,23 @@ describe("TableFilter Specs", function() {
     $("#_tableFilterToggleBtn").click();
     expect($("#_tableFilterList")).not.toBeHidden();
   });
+  
+  it("all checkbox start checked", function(){
+    $("#_tableFilterList input[type='checkbox']").each(function(){
+      expect($(this)).toBeChecked();   
+    });
+  });
+  
+  it("should have all unique items on filter list, column name", function(){
+    expect($("#_tableFilterList input[value='Hugo']")).toExist();
+    expect($("#_tableFilterList input[value='Jose']")).toExist();
+    expect($("#_tableFilterList input[value='TestName']")).toExist();
+    expect($("#_tableFilterList input[value='OtherName']")).not.toExist();
+  });
 
   it("uncheck all on column 1", function() {
     $("#_tableFilterToggleBtn").click();
-    $("#_tableFilterList input.col1.checkall").click(); //uncheck
+    $("#_tableFilterList input.col0.checkall").click(); //uncheck
     expect($("._tablefilter_row0")).toBeHidden();
     expect($("._tablefilter_row1")).toBeHidden();
     expect($("._tablefilter_row2")).toBeHidden();
@@ -33,8 +46,8 @@ describe("TableFilter Specs", function() {
 
   it("check all on column 1", function() {
     $("#_tableFilterToggleBtn").click();
-    $("#_tableFilterList input.col1.checkall").click(); //uncheck
-    $("#_tableFilterList input.col1.checkall").click(); //check
+    $("#_tableFilterList input.col0.checkall").click(); //uncheck
+    $("#_tableFilterList input.col0.checkall").click(); //check
     expect($("._tablefilter_row0")).not.toBeHidden();
     expect($("._tablefilter_row1")).not.toBeHidden();
     expect($("._tablefilter_row2")).not.toBeHidden();
